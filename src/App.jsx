80c9247d8ed18a7ee5626d8fb69e7d843529c6fb
Nitish kumar <nitish.edu.in@gmail.com>
2025-02-06 21:30:45 +0530

    Initialise all folders

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+        <header className='text-center text-blue-600 bg-fuchsia-600'>
+          <h1>E-LIBRARY HUB</h1>
+        </header>
+    </>
+  )
+}
+
+export default App
