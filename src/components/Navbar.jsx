202e42dbbd7c038cfa980fd5ab3a06669865c551
Nitish kumar <nitish.edu.in@gmail.com>
2025-02-14 13:29:46 +0530

    Navbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <nav className="p-4 bg-gray-800 text-white flex justify-between">
+      <h1 className="text-xl font-bold">Online Library</h1>
+      <div className="flex gap-4">
+        <Link to="/" className="hover:underline">
+          Home
+        </Link>
+        <Link to="/browse" className="hover:underline">
+          Browse Books
+        </Link>
+        <Link to="/add" className="hover:underline">
+          Add Book
+        </Link>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
+
+// Here, we have created a simple Navbar component that contains a navigation bar with three links: Home, Browse Books, and Add Book.
+// 4. Create the Home Component
+// Next, we will create the Home component. This component will display a welcome message and a brief description of the application.
